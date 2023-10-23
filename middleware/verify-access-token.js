3885bff94e98cff8865f07ef2854a735ee7c510d
hrach <hrachgalstyann@gmail.com>
2023-10-23 23:54:47 +0400

    WIP: Check RevokedTokens

--- middleware/verify-access-token.js
+++ middleware/verify-access-token.js
@@ -1,11 +1,21 @@
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
+const { RevokedTokens } = require('../db');
 
 // This is a protected route
 module.exports = catchAsync(async (req, res, next) => {
   const accessToken = req.cookies.accessToken;
   if (!accessToken) {
     return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  // Check if token has been revoked
+  const revokedTokens = await RevokedTokens.findOne({
+    where: { accessToken },
+  });
+
+  if (revokedTokens) {
+    return res.status(401).json({ error: 'Token has already been revoked' });
   }
 
   jwt.verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET, (err, user) => {
